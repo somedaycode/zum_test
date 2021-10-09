6f8dd05a2a3f6f456d2c8607bc1bdb35e137ead9
somedaycode <somedaycode@gmail.com>
2021-10-10 02:52:32 +0900

    style: pageStore 초기 값 변경

--- client/src/store/pages.ts
+++ client/src/store/pages.ts
@@ -1,10 +1,9 @@
 import { initState } from '@src/lib/observer';
-import HomePage from '@src/pages/HomePage';
 
 const pageStore = initState({
   key: 'pageStore',
   value: {
-    CurrentPage: HomePage,
+    CurrentPage: null,
   },
 });
 
