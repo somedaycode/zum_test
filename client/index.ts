61f7e473d6670b57c7e431a99e89923965452cde
somedaycode <somedaycode@gmail.com>
2021-10-10 02:13:05 +0900

    feat: 페이지 상태를 구독하고 렌더링하는 App.ts 컴포넌트 추가

--- client/index.ts
+++ client/index.ts
@@ -1,10 +1,17 @@
 import './index.scss';
+import { _ } from '@src/utils/myUtils';
+import Router from '@src/lib/Router';
+import { routesConfig } from '@src/configs/routes';
+
 import App from '@src/App';
 
-export const root = document.querySelector('#root') as HTMLElement;
+export const $root = _.$('#root', document);
+export const router = new Router(routesConfig);
+
+router.init($root);
 
 function init() {
-  new App(root);
+  new App($root);
 }
 
 init();
