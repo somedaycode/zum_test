aff9d5bdcae088e023f15cf4a9184671c9c532bc
somedaycode <somedaycode@gmail.com>
2021-10-10 02:56:55 +0900

    style: index.ts에 router 추가

--- client/index.ts
+++ client/index.ts
@@ -1,14 +1,14 @@
 import './index.scss';
-import { _ } from '@src/utils/myUtils';
+
 import Router from '@src/lib/Router';
 import { routesConfig } from '@src/configs/routes';
+import { _ } from '@src/utils/myUtils';
+import { pageStore } from '@src/store/pages';
 
 import App from '@src/App';
 
 export const $root = _.$('#root', document);
-export const router = new Router(routesConfig);
-
-router.init($root);
+export const router = new Router(routesConfig, pageStore);
 
 function init() {
   new App($root);
