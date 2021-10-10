fbc602a34ad8f72b187a518370bd112ffee548fe
somedaycode <somedaycode@gmail.com>
2021-10-11 03:24:22 +0900

    feat: 컨텐츠 데이터에 제목을 추가 및 타입 지정

--- client/src/store/contents.ts
+++ client/src/store/contents.ts
@@ -1,23 +1,38 @@
 import { initState } from '@src/lib/observer';
 
+export type ContentsState = {
+  isLoading: boolean;
+  title: string;
+  data: HubContent[];
+};
+
+export type HubContent = {
+  idx: Number;
+  mediaName: String;
+  title: String;
+  summaryContent: String;
+  url: String;
+  imageUrl: String;
+};
+
 const lifeData = initState({
   key: 'lifeData',
-  value: { isLoading: true, data: [] },
+  value: { isLoading: true, title: '라이프', data: [] },
 });
 
 const cultureData = initState({
   key: 'cultureData',
-  value: { isLoading: true, data: [] },
+  value: { isLoading: true, title: '컬쳐', data: [] },
 });
 
 const foodData = initState({
   key: 'foodData',
-  value: { isLoading: true, data: [] },
+  value: { isLoading: true, title: '음식', data: [] },
 });
 
 const travelData = initState({
   key: 'travelData',
-  value: { isLoading: true, data: [] },
+  value: { isLoading: true, title: '여행', data: [] },
 });
 
 export { lifeData, cultureData, foodData, travelData };
