112e2fb6a4dce93e37e40ccbb96d941d6636a3f3
somedaycode <somedaycode@gmail.com>
2021-10-11 23:23:07 +0900

    feat: 실시간 랭킹 데이터를 받아온 후 출력하는 Ranking 컴포넌트 추가

--- client/src/components/home/Rankings.ts
+++ client/src/components/home/Rankings.ts
@@ -1,18 +1,66 @@
 import Component from '@src/core/Component';
+import { getState, setState } from '@src/lib/observer';
+
+import { getRankingListApi } from '@src/api/rankingApi';
+
+import { rankingData } from '@src/store/rankings';
+import type { RankingContent, RankingState } from '@src/store/rankings';
 
 export default class Rankings extends Component {
+  constructor($target: HTMLElement) {
+    super($target);
+    this.componentId = 'Rankings';
+    this.keys = [rankingData];
+    this.subscribe();
+  }
+
   htmlTemplate() {
+    const { isLoading, data: rankingList } =
+      getState<RankingState>(rankingData);
+
+    const rankingListTemplate = this.getRankingListTemplate(rankingList);
+
     return `
       <h2 class="ranking__h2">실시간 TOP 12</h2>
-      <ul class="ranking-container">
-        <li class="ranking__list">
-          <div class="ranking-text-wrap">
-            <h3>10억년 신비 품은 해변 신비 품은 해변 신비 품은 해변 따라 삼각산 돌아보니 황제의 氣 꿈틀</h3>
-            <span class="ranking__span-media">media</span>
-            <span class="rank-number">1</span>
-          </div>
-        </li>
-      </ul>
-    `;
+      ${
+        isLoading
+          ? '<div class="loading-spinner"><div>'
+          : `<ul class="ranking-container">
+              ${rankingListTemplate}
+             </ul>`
+      }`;
+  }
+
+  async initializeState() {
+    this.loadingRankingList();
+    const setRankingList = setState(rankingData);
+    const rankingList = await getRankingListApi();
+    setRankingList({
+      isLoading: false,
+      data: rankingList,
+    });
+  }
+
+  loadingRankingList() {
+    const setRankingList = setState(rankingData);
+    setRankingList({
+      isLoading: true,
+      data: [],
+    });
+  }
+
+  getRankingListTemplate(rankingList: RankingContent[]) {
+    return rankingList
+      .map(
+        ({ idx, mediaName, title, url }: RankingContent, rank: number) => `
+      <li class="ranking__list" data-id=${idx} data-url=${url}>
+        <div class="ranking-text-wrap">
+          <h3>${title}</h3>
+          <span class="ranking__span-media">${mediaName}</span>
+          <span class="rank-number">${rank + 1}</span>
+        </div>
+      </li>`
+      )
+      .join('');
   }
 }
