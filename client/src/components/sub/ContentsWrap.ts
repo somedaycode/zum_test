ea96cb03370c80a156a9c5259aa18a82609095ff
somedaycode <somedaycode@gmail.com>
2021-10-12 04:41:38 +0900

    feat: 무한스크롤을 위한 ContentsWrap UI 작성 및 함수 추가

--- client/src/components/sub/ContentsWrap.ts
+++ client/src/components/sub/ContentsWrap.ts
@@ -1,9 +1,16 @@
 import Component from '@src/core/Component';
 import { getState, setState } from '@src/lib/observer';
 import { HubContent } from '@src/store/contents';
+import { _ } from '@src/utils/myUtils';
 import Contents from '../common/Contents';
 
-export default class ContentsWrap extends Component<HubContent[]> {
+type ContentsWrapProps = {
+  page: number;
+  data: HubContent[];
+  setContent: (page: number) => void;
+};
+
+export default class ContentsWrap extends Component<ContentsWrapProps> {
   mountChildComponent() {
     const path = location.pathname.replace('/', '');
     const title = ((path: string) => {
@@ -15,9 +22,45 @@ export default class ContentsWrap extends Component<HubContent[]> {
         favorites: '즐겨찾기',
       }[path];
     })(path);
+    const { data } = this.props;
 
-    if (title) new Contents(this.$target, { title, data: this.props });
+    if (title) new Contents(this.$target, { title, data });
   }
 
-  useEffect() {}
+  useEffect() {
+    if (this.props.data.length >= 40) return;
+    const io = this.getIntersectionObserver();
+    this.observeLastRow(io);
+  }
+
+  observeLastRow(intersectionObserver: IntersectionObserver) {
+    const contentsCards = _.$All('.contents-card', this.$target);
+    contentsCards.forEach((value: Element) => {
+      if (!value.nextElementSibling) intersectionObserver.observe(value);
+    });
+  }
+
+  getIntersectionObserver() {
+    const observerOption = {
+      root: this.$target,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    const io = new IntersectionObserver((entries, observer) => {
+      entries.forEach(async (entry) => {
+        if (entry.isIntersecting) {
+          this.updateContents(this.props.page);
+          observer.unobserve(entry.target);
+          this.observeLastRow(observer);
+        }
+      });
+    }, observerOption);
+
+    return io;
+  }
+
+  updateContents(page: number) {
+    this.props.setContent(page + 1);
+  }
 }
