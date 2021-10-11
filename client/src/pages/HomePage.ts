73c6063524eb53b18ee81e5d311216f9e380181f
somedaycode <somedaycode@gmail.com>
2021-10-12 01:17:39 +0900

    feat: 페이지 이동 후 다시 home으로 왔을 때 클라이언트 캐시 사용

--- client/src/pages/HomePage.ts
+++ client/src/pages/HomePage.ts
@@ -14,6 +14,7 @@ import { getAllContents } from '@src/api/contentsApi';
 
 import Rankings from '@src/components/home/Rankings';
 import Contents from '@src/components/common/Contents';
+import { myCache } from '@src/utils/cache';
 
 export default class HomePage extends Component {
   constructor($target: HTMLDivElement) {
@@ -48,21 +49,11 @@ export default class HomePage extends Component {
   }
 
   mountChildComponent() {
+    const categories = ['라이프', '컬처', '푸드', '여행'];
     const $allContents = _.$All('.contents-wrap', this.$target);
-    $allContents.forEach(($contentsWrap: any, idx: number) => {
-      if (this.keys.length === 0) return this.loadingContents($contentsWrap);
-
-      const contents = getState<ContentsState>(this.keys[idx]);
-      const max = 4;
-      const topContentsWithInFour = contents.data.filter(
-        (_, i: number) => i < max
-      );
-      new Contents($contentsWrap, {
-        ...contents,
-        isLoading: contents.isLoading,
-        data: topContentsWithInFour,
-      });
-    });
+    const { cache } = myCache.get(categories[categories.length - 1]);
+    if (cache?.length > 0) this.loadCachedContents($allContents, categories);
+    else this.loadCategoryContents($allContents, categories);
 
     const $rankings = _.$('.rankings-wrap', this.$target);
     new Rankings($rankings);
@@ -73,6 +64,36 @@ export default class HomePage extends Component {
       title: '',
       isLoading: true,
       data: [],
+    });
+  }
+
+  loadCachedContents($allContents: NodeList, categories: string[]) {
+    $allContents.forEach(($contentsWrap: any, idx: number) => {
+      const { cache } = myCache.get(categories[idx]);
+      new Contents($contentsWrap, {
+        title: categories[idx],
+        isLoading: false,
+        data: cache,
+      });
+    });
+  }
+
+  loadCategoryContents($allContents: NodeList, categories: string[]) {
+    $allContents.forEach(($contentsWrap: any, idx: number) => {
+      if (this.keys.length === 0) return this.loadingContents($contentsWrap);
+
+      const contents = getState<ContentsState>(this.keys[idx]);
+      const max = 4;
+      const topContentsWithInFour = contents.data.filter(
+        (_, i: number) => i < max
+      );
+
+      myCache.set(categories[idx], [...topContentsWithInFour]);
+      new Contents($contentsWrap, {
+        ...contents,
+        isLoading: contents.isLoading,
+        data: topContentsWithInFour,
+      });
     });
   }
 }
