a897652eac8234994f0311b36405c55c2ed0b5e0
somedaycode <somedaycode@gmail.com>
2021-10-11 23:33:34 +0900

    style: 서브페이지 컴포넌트 UI 기본 틀 추가
    * 문화, 음식, 생활, 여행 페이지

---
+++ client/src/pages/CulturePage.ts
@@ -0,0 +1,9 @@
+import Component from '@src/core/Component';
+
+export default class CulturePage extends Component {
+  htmlTemplate() {
+    return `
+      <main class="main-wrap"><main>
+    `;
+  }
+}
---
+++ client/src/pages/TravelPage.ts
@@ -0,0 +1,9 @@
+import Component from '@src/core/Component';
+
+export default class TravelPage extends Component {
+  htmlTemplate() {
+    return `
+      <main class="main-wrap"><main>
+    `;
+  }
+}
