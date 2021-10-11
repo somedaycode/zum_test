0ff28f34f0baa89756e092d4e5e2310f2982dc9a
somedaycode <somedaycode@gmail.com>
2021-10-12 01:19:09 +0900

    chore: 라우터 경로 환경 추가

--- client/src/configs/routes.ts
+++ client/src/configs/routes.ts
@@ -1,13 +1,25 @@
+import CulturePage from '@src/pages/CulturePage';
 import ErrorPage from '@src/pages/ErrorPage';
+import FoodPage from '@src/pages/FoodPage';
 import HomePage from '@src/pages/HomePage';
+import LifePage from '@src/pages/LifePage';
+import TravelPage from '@src/pages/TravelPage';
 
 export type RoutesStore = {
   '/': typeof HomePage;
+  '/lifes': typeof LifePage;
+  '/travels': typeof TravelPage;
+  '/foods': typeof FoodPage;
+  '/cultures': typeof CulturePage;
   '/error': typeof ErrorPage;
   [key: string]: any;
 };
 
 export const routesConfig: RoutesStore = {
   '/': HomePage,
+  '/lifes': LifePage,
+  '/travels': TravelPage,
+  '/foods': FoodPage,
+  '/cultures': CulturePage,
   '/error': ErrorPage,
 };
