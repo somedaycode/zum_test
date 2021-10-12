671e2a8aec02a0e264069306d029985cb472799e
somedaycode <somedaycode@gmail.com>
2021-10-12 20:38:18 +0900

    feat: 상세페이지로 이동시 서버에 데이터 요청

--- client/src/pages/DetailPage.ts
+++ client/src/pages/DetailPage.ts
@@ -1,9 +1,27 @@
 import Component from '@src/core/Component';
 
+import { contentsDetailData } from '@src/store/contentsDetail';
+import { getDetailContents } from '@src/api/detailContents';
+
 export default class DetailPage extends Component {
+  constructor($target: HTMLElement) {
+    super($target);
+    this.componentId = 'DetailPage';
+    this.keys = [contentsDetailData];
+    this.subscribe();
+  }
+
   htmlTemplate() {
     return `
       여기는 상세페이지 입니다.
     `;
+  }
+
+  async initializeState() {
+    const url = 'https://hub.zum.com';
+    const params = location.pathname.replace('/detail', '');
+    const originalUrl = url + params;
+    const data = await getDetailContents(originalUrl);
+    console.log(data);
   }
 }
