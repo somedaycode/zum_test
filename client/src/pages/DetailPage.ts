008200e5c76b3fdb4eedd6b416f0e6712b2d2b71
somedaycode <somedaycode@gmail.com>
2021-10-12 21:45:04 +0900

    feat: 상세정보 요청 후 렌더링

--- client/src/pages/DetailPage.ts
+++ client/src/pages/DetailPage.ts
@@ -2,6 +2,7 @@ import Component from '@src/core/Component';
 
 import { contentsDetailData } from '@src/store/contentsDetail';
 import { getDetailContents } from '@src/api/detailContents';
+import { getState, setState } from '@src/lib/observer';
 
 export default class DetailPage extends Component {
   constructor($target: HTMLElement) {
@@ -12,16 +13,36 @@ export default class DetailPage extends Component {
   }
 
   htmlTemplate() {
+    const originalUrl = this.getOriginalUrl();
+    const contentsHTML = getState<any>(contentsDetailData);
+    const html = contentsHTML[originalUrl];
+    if (!html) return '<div class="loading-spinner"></div>';
+
+    const { title, writer, body } = html;
     return `
-      여기는 상세페이지 입니다.
+    <main class="main-wrap">
+      <h2>${title}</h2>
+      <p>${writer}</p>
+      <section>${body}</section>
+    </main>
     `;
   }
 
   async initializeState() {
+    const originalUrl = this.getOriginalUrl();
+    const data = await getDetailContents(originalUrl);
+    const currentContents = getState<any>(contentsDetailData);
+    const setDetailContents = setState(contentsDetailData);
+    setDetailContents({
+      ...currentContents,
+      [originalUrl]: data,
+    });
+  }
+
+  getOriginalUrl() {
     const url = 'https://hub.zum.com';
     const params = location.pathname.replace('/detail', '');
     const originalUrl = url + params;
-    const data = await getDetailContents(originalUrl);
-    console.log(data);
+    return originalUrl;
   }
 }
