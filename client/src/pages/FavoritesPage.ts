dee08b1c349157204ad7cf8caa2812fc1b649383
somedaycode <somedaycode@gmail.com>
2021-10-13 17:58:37 +0900

    feat: 즐겨찾기 페이지에 등록한 콘텐츠 렌더링

--- client/src/pages/FavoritesPage.ts
+++ client/src/pages/FavoritesPage.ts
@@ -1,11 +1,21 @@
+import Contents from '@src/components/common/Contents';
 import Component from '@src/core/Component';
+import { _ } from '@src/utils/myUtils';
 
 export default class FavoritesPage extends Component {
   htmlTemplate() {
     return `
       <main class="main-wrap">
-        <section class="contents-wrap">즐겨찾기 페이지</section>
+        <section class="contents-wrap"></section>
       <main>
     `;
+  }
+
+  mountChildComponent() {
+    const contentsWrap = _.$('.contents-wrap', this.$target);
+    const savedContents = localStorage.getItem('favorites');
+    if (!savedContents) return;
+    const favoriteContents = JSON.parse(savedContents);
+    new Contents(contentsWrap, { title: '즐겨찾기', data: favoriteContents });
   }
 }
